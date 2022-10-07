88741f5fa0d167690f74a57bb601cd30a17b717f
Scighost <scighost@outlook.com>
2022-10-07 17:49:10 +0800

    最新生成版更新渠道

--- Pica3/Pages/SettingPage.xaml.cs
+++ Pica3/Pages/SettingPage.xaml.cs
@@ -2,7 +2,11 @@
 using Microsoft.UI.Xaml.Controls;
 using Pica3.Controls;
 using Scighost.WinUILib.Helpers;
+using System;
+using System.Diagnostics;
 using System.IO;
+using System.Net.Http;
+using System.Runtime.InteropServices;
 using Windows.Storage;
 using Windows.Storage.Pickers;
 using Windows.System;
@@ -522,6 +526,88 @@ public sealed partial class SettingPage : Page
     partial void OnApplicationSingletonChanged(bool value)
     {
         AppSetting.TrySetValue(SettingKeys.EnableApplicationSingleton, value);
+    }
+
+
+
+    [RelayCommand]
+    private async Task CheckComicBuildVersionAsync()
+    {
+        try
+        {
+            const string URL = "https://os.scighost.com/pica3/build/bika3_latest_x64.zip";
+            const string SCRIPT = """
+                try {
+                    Write-Host "哔咔 3 最新生成版 下载脚本"
+                    Write-Host "注意：最新生成版不是正式发布版，可能存在功能缺失，Bug 频出的问题。" -ForegroundColor Yellow
+                    Write-Host "`n`n`n"
+                    Write-Host "当前路径：$(Get-Location)"
+                    $null = New-Item ./temp -ItemType Directory -Force -ErrorAction Stop
+                    $result = Invoke-WebRequest https://os.scighost.com/pica3/build/bika3_latest_x64.zip -Method HEAD -ErrorAction Stop
+                    $version = $result.Headers['x-oss-meta-version']
+                    $size = ($result.Headers['Content-Length']/1024/1024).ToString('F2')
+                    Write-Host "下载安装包（版本：$version，大小：$size MB）"
+                    Invoke-WebRequest https://os.scighost.com/pica3/build/bika3_latest_x64.zip -OutFile ./temp/bika3_latest.zip -ErrorAction Stop
+                    Write-Host "开始解压"
+                    Expand-Archive -Path ./temp/bika3_latest.zip -DestinationPath ./temp/ -Force -ErrorAction Stop
+                    try {
+                        $null = Get-Process -Name bika3 -ErrorAction Stop
+                        Write-Host "bika3.exe 正在运行，等待进程退出" -ForegroundColor Yellow
+                        Wait-Process -Name bika3 -ErrorAction Stop
+                        Start-Sleep -Seconds 1
+                    } catch { }
+                    Write-Host "替换文件"
+                    Get-ChildItem -Path ./temp/bika3/* -File | Move-Item -Destination ./ -Force -ErrorAction Stop
+                    Copy-Item -Path ./temp/bika3/* -Destination ./ -Force -Recurse -ErrorAction Stop
+                    Write-Host "清理安装包"
+                    Remove-Item -Path ./temp -Force -Recurse -ErrorAction Stop
+                    Write-Host "更新完成"
+                    Invoke-Item -Path ./bika3.exe -ErrorAction Stop
+                    Start-Sleep -Seconds 2
+                } catch {
+                    Write-Host $_.Exception -ForegroundColor Red -BackgroundColor Black
+                    Write-Host "任意键退出"
+                    [Console]::ReadKey()
+                }
+                """;
+            var replace = RuntimeInformation.ProcessArchitecture switch
+            {
+                Architecture.X64 => false,
+                Architecture.Arm64 => true,
+                _ => throw new PlatformNotSupportedException($"不支持 {RuntimeInformation.ProcessArchitecture}"),
+            };
+            var url = replace ? URL.Replace("x64", "arm64") : URL;
+            var result = await new HttpClient().GetAsync(url, HttpCompletionOption.ResponseHeadersRead);
+            if (result.Headers.TryGetValues("x-oss-meta-version", out var values))
+            {
+                if (Version.TryParse(values.FirstOrDefault(), out var version))
+                {
+                    if (version > typeof(App).Assembly.GetName().Version)
+                    {
+                        var dialog = new ContentDialog
+                        {
+                            Title = $"新版本 {version}",
+                            Content = "最新生成版不是正式发布版，可能存在功能缺失，Bug 频出的问题。\n下载过程中可继续使用，关闭应用后自动更新。",
+                            IsPrimaryButtonEnabled = true,
+                            PrimaryButtonText = "下载",
+                            CloseButtonText = "取消",
+                            DefaultButton = ContentDialogButton.Close,
+                            XamlRoot = MainWindow.Current.XamlRoot,
+                        };
+                        if (await dialog.ShowWithZeroMarginAsync() is ContentDialogResult.Primary)
+                        {
+                            Process.Start("PowerShell", replace ? SCRIPT.Replace("x64", "arm64") : SCRIPT);
+                        }
+                        return;
+                    }
+                }
+            }
+            NotificationProvider.Success("已是最新版本");
+        }
+        catch (Exception ex)
+        {
+            ex.HandlePicaException();
+        }
     }
 
 
---
+++ Pica3/bika3Update.ps1
@@ -0,0 +1,32 @@
+﻿try {
+    Write-Host "哔咔 3 最新生成版 下载脚本"
+    Write-Host "注意：最新生成版不是正式发布版，可能存在功能缺失，Bug 频出的问题。" -ForegroundColor Yellow
+    Write-Host "`n`n`n"
+    Write-Host "当前路径：$(Get-Location)"
+    $null = New-Item ./temp -ItemType Directory -Force -ErrorAction Stop
+    $result = Invoke-WebRequest https://os.scighost.com/pica3/build/bika3_latest_x64.zip -Method HEAD -ErrorAction Stop
+    $version = $result.Headers['x-oss-meta-version']
+    $size = ($result.Headers['Content-Length']/1024/1024).ToString('F2')
+    Write-Host "下载安装包（版本：$version，大小：$size MB）"
+    Invoke-WebRequest https://os.scighost.com/pica3/build/bika3_latest_x64.zip -OutFile ./temp/bika3_latest.zip -ErrorAction Stop
+    Write-Host "开始解压"
+    Expand-Archive -Path ./temp/bika3_latest.zip -DestinationPath ./temp/ -Force -ErrorAction Stop
+    try {
+        $null = Get-Process -Name bika3 -ErrorAction Stop
+        Write-Host "bika3.exe 正在运行，等待进程退出" -ForegroundColor Yellow
+        Wait-Process -Name bika3 -ErrorAction Stop
+        Start-Sleep -Seconds 1
+    } catch { }
+    Write-Host "替换文件"
+    Get-ChildItem -Path ./temp/bika3/* -File | Move-Item -Destination ./ -Force -ErrorAction Stop
+    Copy-Item -Path ./temp/bika3/* -Destination ./ -Force -Recurse -ErrorAction Stop
+    Write-Host "清理安装包"
+    Remove-Item -Path ./temp -Force -Recurse -ErrorAction Stop
+    Write-Host "更新完成"
+    Invoke-Item -Path ./bika3.exe -ErrorAction Stop
+    Start-Sleep -Seconds 2
+} catch {
+    Write-Host $_.Exception -ForegroundColor Red -BackgroundColor Black
+    Write-Host "任意键退出"
+    [Console]::ReadKey()
+}
